9fd10eabf05cf1e5269f875c388535f5247d80ba
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-06-22 14:05:04 +0100

    Migration for openshift

--- wsgi/application.py
+++ wsgi/application.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SDC.settings")
+## GETTING-STARTED: make sure the next line points to your settings.py:
+os.environ['DJANGO_SETTINGS_MODULE'] = 'SDC.settings'
+## GETTING-STARTED: make sure the next line points to your django project dir:
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'src'))
+virtenv = os.environ['APPDIR'] + '/virtenv/'
+## GETTING-STARTED: make sure the next line has the right python version:
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except:
+    pass
 
 application = get_wsgi_application()
