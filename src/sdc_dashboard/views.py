b7127908a9e83c617dc2d4ee5e937b861e60b753
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-06-22 20:30:00 +0100

    Query for multiple returned entries

--- src/sdc_dashboard/views.py
+++ src/sdc_dashboard/views.py
@@ -20,8 +20,8 @@ def get_user_data(request):
 		return HttpResponse(json.dumps({'message':'authentication error', 'code': 401}), content_type="application/json")
 
 	skydiver = SkyDiver.objects.get(username=current_user.username)
-	sessionData = SessionData.objects.get(skyDiver=skydiver)
-	data_file = open(settings.DATA_DIR + "/" + sessionData.location)
+	sessionData = SessionData.objects.filter(skyDiver=skydiver)
+	data_file = open(settings.DATA_DIR + "/" + sessionData[len(sessionData) - 1].location)
 	data = data_file.readline()
 	data_file.close()
 	return HttpResponse(data, content_type="application/json")
