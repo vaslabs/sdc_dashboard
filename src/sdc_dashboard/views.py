00da027dd605b1e18aea1015938c8f76553a23e9
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-06-22 15:00:48 +0100

    Missing import

--- src/sdc_dashboard/views.py
+++ src/sdc_dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import SkyDiver, SessionData
 import json
+from django.http import HttpResponse
 from SDC import settings
 # Create your views here.
 def index(request):
@@ -16,7 +17,7 @@ def user_dashboard(request):
 def get_user_data(request):
 	current_user = request.user
 	if (not current_user.is_authenticated()):
-		return HttpResponse(json.dumps({'message':'authentication error', 'code': 401}), content_type="application/json")
+		return HttpResponse(simplejson.dumps({'message':'authentication error', 'code': 401}), content_type="application/json")
 
 	skydiver = SkyDiver.objects.get(username=current_user.username)
 	sessionData = SessionData.objects.get(skyDiver=skydiver)
