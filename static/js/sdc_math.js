ad4be17a279494fa384fb2be13b6a9af623403f5
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-06-26 08:12:47 +0100

    Fixed hung issue

--- static/js/sdc_math.js
+++ static/js/sdc_math.js
@@ -3,16 +3,14 @@ function calculateVerticalVelocity(data, sample_skip) {
   var initialTime = data[0].timestamp;
   var velocities = [];
   var dx, dt;
-  for (var i = 1; i < data.length - 1; i+=sample_skip) {
+  for (var i = 1; i < data.length - 1; i+=(sample_skip+1)) {
     t1 = data[i-1].timestamp - initialTime;
     t2 = data[i].timestamp - initialTime;
     x1 = data[i-1].altitude;
     x2 = data[i].altitude;
     dt = (t2-t1)/1000;
-    dx = (x1-x2);
+    dx = (x2-x1);
     var u = dx/dt;
-    if (u < 0)
-      console.log(t2, t1, x1, x2);
     velocities.push([t2/1000, u]);
   }
 
