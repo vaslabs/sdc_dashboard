ce5b293b02bddd2ed34dbe747b2e4615758d6a7b
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-08-13 20:27:56 +0100

    Settings template for mysql

--- src/SDC/settings.py
+++ src/SDC/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'SDC.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'sdc-dashboard-db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
