011961a9138ebc137abd1d52aadafbbf6f82c715
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-08-16 14:52:59 +0100

    Logbook model

--- src/sdc_dashboard/models.py
+++ src/sdc_dashboard/models.py
@@ -22,3 +22,18 @@ class ShareLink(models.Model):
 
     def __str__(self):
         return "/shared_session/" + self.shareLink
+
+class Location(models.Model):
+    latitude = models.DecimalField(max_digits=12, decimal_places=9)
+    longitude = models.DecimalField(max_digits=12, decimal_places=9)
+    name = models.CharField(max_length=255)
+
+class Logbook(models.Model):
+    skyDiver = models.ForeignKey(SkyDiver)
+    sessionData = models.ForeignKey(SessionData)
+    location = models.ForeignKey(Location)
+
+    freeFallTime = models.FloatField()
+    exitAltitude = models.FloatField()
+    deploymentAltitude = models.FloatField()
+    maxVerticalVelocity = models.FloatField()
