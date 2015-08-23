e430c3c5a0a2b27a7739ea0e12583ee359e35c0e
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-08-23 16:32:23 +0100

    Validates token and username - bit of refactoring

--- src/sdc_invites/urls.py
+++ src/sdc_invites/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+   url(r'^username/$', views.validate_new_username, name='validate_new_username'),
+   url(r'^validate_invitation_token/$', views.validate_invitation_token, name='validate_invitation_token'),
    url(r'^$', views.default, name='invites_default'),
+
 ]
