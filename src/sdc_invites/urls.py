d924933c240f8c9782c1c2e3f4b95ffaede53554
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-08-23 17:04:36 +0100

    All validators in place, ready for registration

--- src/sdc_invites/urls.py
+++ src/sdc_invites/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
    url(r'^username/$', views.validate_new_username, name='validate_new_username'),
    url(r'^validate_invitation_token/$', views.validate_invitation_token, name='validate_invitation_token'),
+   url(r'^email/$', views.validate_email, name='validate_email'),
+   url(r'^password/$', views.validate_password, name='validate_password'),
    url(r'^$', views.default, name='invites_default'),
 
 ]
