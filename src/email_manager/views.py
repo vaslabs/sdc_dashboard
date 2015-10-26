6899de40f3dbf2f7eacf3f0ae7a74e9948eef074
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-10-26 18:53:11 +0000

    User activation through url

--- src/email_manager/views.py
+++ src/email_manager/views.py
@@ -1,1 +1,19 @@
 from django.shortcuts import render
+from sdc_invites.models import ActivationToken
+from sdc_dashboard.views import index
+from sdc_dashboard.models import SkyDiver
+
+def activate_account(request, token):
+	activationToken = ActivationToken.objects.get(token=token)
+	if (activationToken.activated):
+		return index(request)
+	activationToken.activated = True
+	activationToken.save()
+
+	username = activationToken.user.username
+	name = username
+	skydiver = SkyDiver(username=username, name=name)
+	skydiver.save()
+	return index(request)
+
+
