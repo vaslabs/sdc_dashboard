e93c9e95fddd6e32702e23bb5165335a3080dc70
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-10-26 19:33:49 +0000

    Create an api token when activating an account

--- src/email_manager/views.py
+++ src/email_manager/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from sdc_invites.models import ActivationToken
 from sdc_dashboard.views import index
 from sdc_dashboard.models import SkyDiver
-
+from rest_framework.authtoken.models import Token
 def activate_account(request, token):
 	activationToken = ActivationToken.objects.get(token=token)
 	if (activationToken.activated):
@@ -14,6 +14,8 @@ def activate_account(request, token):
 	name = username
 	skydiver = SkyDiver(username=username, name=name)
 	skydiver.save()
+	apiToken = Token(user=activationToken.user)
+	apiToken.save()
 	return index(request)
 
 
