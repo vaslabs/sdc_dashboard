bd264d3313d3385e84b41d123a4e6e8a39a0bc07
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-10-26 22:02:49 +0000

    path fix

--- src/SDC/settings.py
+++ src/SDC/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT=BASE_DIR+'wsgi/static/'
+STATIC_ROOT=BASE_DIR+'/wsgi/static/'
 STATIC_URL = '/static/'
 
 LOGIN_URL = reverse_lazy('login')
