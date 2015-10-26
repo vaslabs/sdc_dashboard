89075e73f935e6a6165327771d0653dcfc3318e6
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-10-26 18:37:53 +0000

    Sends activation email after registration

--- src/sdc_invites/models.py
+++ src/sdc_invites/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from sdc_dashboard.models import SkyDiver
+from django.contrib.auth.models import User
 import calendar
 import json
+from datetime import datetime
 
 class Invitation(models.Model):
 	skydiver = models.ForeignKey(SkyDiver)
@@ -13,3 +15,14 @@ class Invitation(models.Model):
     
 	def __str__(self):
 		return self.token
+
+class ActivationToken(models.Model):
+	token = models.CharField(max_length=32, unique=True)
+	user = models.ForeignKey(User)
+	createdDate = models.DateTimeField("date created")
+	activated = models.BooleanField()
+	def save(self):
+		if not self.id:
+			self.createdDate = datetime.now().replace(tzinfo=None)
+			self.activated = False
+			super(ActivationToken, self).save()
