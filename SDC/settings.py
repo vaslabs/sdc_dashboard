459702ba9b54e120e4323d44bb7e0186f7abc6c2
Vasilis Nicolaou <vaslabs@yahoo.gr>
2015-10-29 18:55:22 +0000

    Missing import

--- SDC/settings.py
+++ SDC/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import socket
+
 from django.core.urlresolvers import reverse_lazy
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
