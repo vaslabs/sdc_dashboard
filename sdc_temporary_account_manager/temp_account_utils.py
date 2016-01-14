fbe2524a889ae8543b7234d7b8fca12b9001364a
Vasilis Nicolaou <vaslabs@yahoo.gr>
2016-01-14 15:57:22 +0000

    Encryption with account public key for responses

--- sdc_temporary_account_manager/temp_account_utils.py
+++ sdc_temporary_account_manager/temp_account_utils.py
@@ -1,6 +1,9 @@
 
 from .models import TemporarySkydiver, TemporaryToken
 import md5
+from Crypto.PublicKey import RSA
+from Crypto.Util import asn1
+from base64 import b64decode, b64encode
 
 def create_temporary_account(publicKey):
 	temporarySkydiver = TemporarySkydiver(publicKey=publicKey)
@@ -13,3 +16,17 @@ def create_temporary_account(publicKey):
 	temporaryApiToken = TemporaryToken(token=apiToken, skydiver=temporarySkydiver)
 	temporaryApiToken.save()
 	return temporarySkydiver, temporaryApiToken
+
+def encrypt(account, data):
+	key = account.publicKey
+	publicKeyObj = generatePublicKey(key)
+	encrypted_data = publicKeyObj.encrypt(data, 123)
+	return b64encode(encrypted_data[0])
+
+
+def generatePublicKey(key64):
+	keyDER = b64decode(key64)
+	seq = asn1.DerSequence()
+	seq.decode(keyDER)
+	keyPub = RSA.construct( (seq[0], seq[1]) )
+	return keyPub
--- sdc_temporary_account_manager/views.py
+++ sdc_temporary_account_manager/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
 # Create your views here.
 
+from django.shortcuts import render
+import json, string, random
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .temp_account_utils import create_temporary_account, encrypt
+
+@csrf_exempt
+def create_account(request):
+	received_json_data=json.loads(request.body)
+	publicKey = received_json_data(received_json_data["publicKey"])
+	account, token = create_temporary_account
+	encrypted_token = encrypt(account, token)
+	response = {}
+	response['apitoken'] = encrypted_token
+	
 
 
