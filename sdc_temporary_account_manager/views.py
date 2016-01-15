d484cd3f7324ecc6e77f64e06970b99744b2583d
Vasilis Nicolaou <vaslabs@yahoo.gr>
2016-01-15 17:03:18 +0000

    Can submit data

--- sdc_temporary_account_manager/views.py
+++ sdc_temporary_account_manager/views.py
@@ -4,14 +4,14 @@ from django.shortcuts import render
 import json, string, random
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from .temp_account_utils import create_temporary_account, encrypt, getAccount
+from .temp_account_utils import create_temporary_account, encrypt, getAccount, saveData
 from rest_framework.decorators import api_view
 
 def ok_message():
 	return HttpResponse("{'message':'OK', 'code': 200}", content_type="application/json")
 
 def authentication_error():
-	return HttpResponse("{'message':'ERROR', 'code': 401", content_type="application/json", status_code=401)
+	return HttpResponse("{'message':'ERROR', 'code': 401", content_type="application/json")
 
 @csrf_exempt
 @api_view(['POST'])
@@ -29,11 +29,20 @@ def create_account(request):
 
 @csrf_exempt
 def save_session(request):
-	apitoken = 	request.META['HTTP_AUTHORIZATION'].split(' ')[1]
-	temporaryUser = temp_account_utils.getAccount(apitoken)
+	if ('HTTP_AUTHORIZATION' not in request.META):
+		return 
+	try:
+		apitoken = 	request.META['HTTP_AUTHORIZATION'].split(' ')[1]
+	except:
+		return authentication_error()
 
+	try:
+		temporaryUser = getAccount(apitoken)
+	except:
+		print "Authentication error: " + apitoken
+		return authentication_error
 	if (temporaryUser != None):
-		temp_account_utils.saveData(temporaryUser, request.body)
+		saveData(temporaryUser, request.body)
 		return ok_message()
 	else:
 		return authentication_error()
