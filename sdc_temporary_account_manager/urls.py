059a4a4f05605fa2c128c745f6216e77a87a8855
Vasilis Nicolaou <vaslabs@yahoo.gr>
2016-01-15 17:03:18 +0000

    Can submit data

--- sdc_temporary_account_manager/urls.py
+++ sdc_temporary_account_manager/urls.py
@@ -5,4 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     url(r'^create_account/', csrf_exempt(views.create_account), name='create_account'),
+    url(r'^submit_session/', csrf_exempt(views.save_session), name='submit_session'),
+
 ]
