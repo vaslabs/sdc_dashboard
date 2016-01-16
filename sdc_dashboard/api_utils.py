7a819972c76d036454e71d0e48cd294e24775ea9
Vasilis Nicolaou <vaslabs@yahoo.gr>
2016-01-16 15:22:49 +0000

    Fixes for real user session list

--- sdc_dashboard/api_utils.py
+++ sdc_dashboard/api_utils.py
@@ -1,9 +1,13 @@
-from .models import SkyDiver
+from .models import SkyDiver, SessionData
 from rest_framework.authtoken.models import Token
 
 def get_skydiver_from_token(request):
 	token_key = request.META['HTTP_AUTHORIZATION'].split(' ')[1]
+	return from_token(token_key)
+
+def from_token(token_key):
 	token = Token.objects.get(key=token_key)
+	print token
 	current_user = token.user
 	skydiver = SkyDiver.objects.get(username=current_user.username)
 
